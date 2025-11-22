83c3f8ce387e40aaca9930b2d567d112e01a239d
zevy3 <pthlp1803@ya.ru>
2025-11-22 22:45:55 +0300

    криво-рабочая версия

--- test_get_chat.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client
-import asyncio
-
-async def main():
-    async with Client("TELERAG-MessageScrapper") as app:
-        chat = await app.get_chat(-1002706500293)
-        print(chat.id, chat.title)
-
-asyncio.run(main())
